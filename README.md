74a4454afd9e2442d1e916fb636445ef6c7f963f
Sean Gephardt <seangephardt@live.com>
2014-11-19 17:26:24 -0800

    Initial Checkin

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Javascript-Utility-Libraries
 ============================
 
-Some random Javascript utility libraries that I'm working one.
+Some random Javascript utility libraries that I'm working on.
---
+++ test.js
@@ -0,0 +1,26 @@
+﻿/* 
+Copyright 2014 Sean Gephardt. http://seangephardt.com/
+Used as a testing function.
+ To be included in a html page.
+ Requires jQuery.
+ */
+"use strict";
+$( function ()
+{
+	try
+	{
+		console.clear();
+		console.info( "BEGIN" );
+
+		/* begin testing of 'supergood' here */
+	}
+	catch ( err )
+	{
+		console.error( err.number, err.name, err.message, err.stack );
+	}
+	finally
+	{
+		console.info( "END" );
+	}
+	return;
+} );
