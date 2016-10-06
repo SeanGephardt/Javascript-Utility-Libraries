1bbc88f856f6494e2210a87beb912855e013bf03
Sean Gephardt <seangephardt@live.com>
2016-10-06 16:43:11 -0700

    sdfs

--- js/js-template.js
+++ js/js-template.js
@@ -1,27 +1,30 @@
 ﻿/* 
-Copyright 2014 Sean Gephardt. http://seangephardt.com/
+Copyright 2017 Sean Gephardt. http://seangephardt.com/
 Used as a testing function.
  To be included in a html page.
  Requires jQuery.
  */
 "use strict";
-$( function ()
+( function ()
 {
 	try
 	{
-		console.clear();
 		console.info( "BEGIN" );
-
-		/* begin testing of 'supergood' here */
-		console.log( "SuperGood.Random.GreyColors()", SuperGood.Random.GreyColors() );
+		window.document.addEventListener( "DOMContentLoaded", function ( str )
+		{
+			console.log( "DOMContentLoaded" );
+			return;
+		} )
 	}
-	catch ( err )
+	catch ( ex )
 	{
-		console.error( err.number, err.name, err.message, err.stack );
+		console.error( ex.number, ":", ex.name, ":", ex.message );
+		console.error( "exception stack::", ex.stack );
+		return;
 	}
 	finally
 	{
 		console.info( "END" );
 	}
 	return;
-} );
+} )();
